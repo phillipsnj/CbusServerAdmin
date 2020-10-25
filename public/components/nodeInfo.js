805c87b3bfc0cf4d5126e21c7e5cd1651d376e6e
phillipsnj <nigeljphillips@yahoo.co.uk>
2020-10-25 21:51:42 +0000

    Updates and Bug Fixes

--- public/components/nodeInfo.js
+++ public/components/nodeInfo.js
@@ -1,8 +1,23 @@
 Vue.component('nodeInfo', {
     name: "nodeInfo",
     //props: ['nodeId'],
+    data: () => ({
+        nodeDetails: {
+            name: "",
+            colour: ""
+        }
+    }),
     mounted() {
         this.$root.send('REQUEST_ALL_NODE_PARAMETERS', {"nodeId": this.node.node, "parameters": 20, "delay":30})
+        console.log(`nodeDetails : ${this.nodeId}`)
+        if (this.nodeId in this.$store.state.layout.nodeDetails) {
+            this.nodeDetails.name = this.$store.state.layout.nodeDetails[this.nodeId].name
+            this.nodeDetails.colour = this.$store.state.layout.nodeDetails[this.nodeId].colour
+            this.nodeDetails.group = this.$store.state.layout.nodeDetails[this.nodeId].group
+        } else {
+            this.nodeDetails.name = `Node ${this.nodeId}`
+            this.nodeDetails.colour = `Black`
+        }
     },
     computed: {
         nodeId: function () {
@@ -13,12 +28,66 @@ Vue.component('nodeInfo', {
         },
         moduleVersion: function () {
             return `${this.node.parameters[7]}.${String.fromCharCode(this.node.parameters[2])}`
+        },
+        displayColour: function () {
+            return this.$store.state.layout.nodeDetails[this.nodeId].colour + '--text'
         }
     },
+    methods: {
+    update() {
+        this.$store.state.layout.nodeDetails[this.nodeId] = {}
+        this.$store.state.layout.nodeDetails[this.nodeId].name = this.nodeDetails.name
+        this.$store.state.layout.nodeDetails[this.nodeId].colour = this.nodeDetails.colour
+        this.$store.state.layout.nodeDetails[this.nodeId].group = this.nodeDetails.group
+        this.$root.send('UPDATE_LAYOUT_DETAILS', this.$store.state.layout)
+    },
+    groupList() {
+        let output = []
+        for (let node in this.$store.state.layout.nodeDetails) {
+            console.log(`grouplist ${node} `)
+            if (this.$store.state.layout.nodeDetails[node].group) {
+                console.log(`groupList :: ${this.$store.state.layout.nodeDetails[node].group}`)
+                output.push(this.$store.state.layout.nodeDetails[node].group)
+            }
+        }
+        return output
+    }
+},
     template: `
       <div>
       <!--<h1>Node Info {{ nodeId }}</h1>--> 
       <p v-if="$store.state.debug">{{ node.parameters }}</p>
+      <v-container>
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field outlined
+                        label="Name"
+                        placeholder="Nama"
+                        v-model=nodeDetails.name
+                        @change="update()"
+                        :text-color="nodeDetails.colour"
+          >
+          </v-text-field>
+          <displayNodeName :id="nodeId"></displayNodeName>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-select
+              label="Colour"
+              v-model="nodeDetails.colour"
+              :items="$store.state.colours"
+              outlined
+              @change="update()"
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-combobox
+              v-model="nodeDetails.group"
+              :items="groupList()"
+              label="Select a group"
+              outlined
+              @change="update()"
+          ></v-combobox>
+        </v-col>
+      </v-container>
       <v-container>
         <v-row>
           <nodeValue name="NodeId" :value="nodeId"></nodeValue>
@@ -36,6 +105,23 @@ Vue.component('nodeInfo', {
           <nodeValue name="Actual Events" :value="Object.values(node.actions).length"></nodeValue>
         </v-row>
       </v-container>
+      </div>`
+})
+
+Vue.component('nodeDetail', {
+    name: "nodeParameter",
+    props: ['nodeId', 'parId', 'name'],
+    template: `
+      <div>
+      <v-card class="xs6 md3 pa-3" flat>
+        <v-text-field
+            :label="name"
+            v-model="$store.state.nodes[nodeId].parameters[parId]"
+            outlined
+            readonly
+        >
+        </v-text-field>
+      </v-card>
       </div>`
 })
 
--- public/components/nodes_list.js
+++ public/components/nodes_list.js
@@ -5,6 +5,8 @@ Vue.component('nodes-list', {
         return {
             headers: [
                 {text: 'node', value: 'node'},
+                {text: 'Name', value: 'name'},
+                {text: 'Group', value: 'group'},
                 {text: 'manuf', value: 'manuf'},
                 {text: 'module', value: 'module'},
                 {text: 'consumer', value: 'consumer'},
@@ -14,6 +16,7 @@ Vue.component('nodes-list', {
                 {text: 'coe', value: 'coe'},
                 {text: 'Actions', value: 'actions', sortable: false}
             ],
+            search: '',
             dialog: false,
             nodeComponent: 'mergDefault',
             selectedNode: {}
@@ -29,41 +32,92 @@ Vue.component('nodes-list', {
             if (node.component) {
                 this.$store.state.display_component = node.component
             } else {
-                this.$store.state.display_component  = 'mergDefault'
+                this.$store.state.display_component = 'mergDefault'
             }
-       }
+        },
+        displayName: function (id) {
+            return this.$store.state.layout.nodeDetails[id].name
+        },
+        displayColour: function (id) {
+            return this.$store.state.layout.nodeDetails[id].colour + '--text'
+        },
+        displayGroup: function (id) {
+            return this.$store.state.layout.nodeDetails[id].group
+        }
     },
     template: `
       <v-container>
-        <v-toolbar flat>
-          <v-toolbar-title>Cbus Modules</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text v-on:click="QNN">Check Nodes</v-btn>
-        </v-toolbar>
-
-        <v-data-table :headers="headers"
-                      :items="Object.values($store.state.nodes)"
-                      item-key="node"
-                      class="elevation-1">
-          <template v-slot:item.flim="{ item }">
-            <v-chip color="amber" dark v-if="item.flim">Flim</v-chip>
-            <v-chip color="green" dark v-else>Slim</v-chip>
-          </template>
-          <template v-slot:item.status="{ item }">
-            <v-chip color="green" dark v-if="item.status">Ok</v-chip>
-            <v-chip color="red" dark v-else>Error</v-chip>
-          </template>
-          <template v-slot:item.actions="{ item }">
-            <v-btn color="blue darken-1" text @click="editNode(item)" outlined>Edit</v-btn>
-          </template>
-        </v-data-table>
-        <div>
-          <v-container v-if="$store.state.debug">
+      <v-data-table :headers="headers"
+                    :items="Object.values($store.state.nodes)"
+                    item-key="node"
+                    class="elevation-1"
+                    :search="search">
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>Modules</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+            ></v-text-field>
+            <v-btn color="blue darken-1" text v-on:click="QNN()">Check Modules</v-btn>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.name="{ item }">
+          <div v-if="item.node in $store.state.layout.nodeDetails"
+               :class="displayColour(item.node)">{{ displayName(item.node) }}</div>
+          <div v-else class="black--text">Node : {{ item.node }} </div>
+        </template>
+        <template v-slot:item.group="{ item }">
+          <div v-if="item.node in $store.state.layout.nodeDetails">{{ displayGroup(item.node) }}</div>
+        </template>
+        <template v-slot:item.flim="{ item }">
+          <v-chip color="amber" dark v-if="item.flim">Flim</v-chip>
+          <v-chip color="green" dark v-else>Slim</v-chip>
+        </template>
+        <template v-slot:item.status="{ item }">
+          <v-chip color="green" dark v-if="item.status">Ok</v-chip>
+          <v-chip color="red" dark v-else>Error</v-chip>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-btn color="blue darken-1" text @click="editNode(item)" outlined>Edit</v-btn>
+        </template>
+      </v-data-table>
+      <div>
+        <v-container v-if="$store.state.debug">
           <h3>Raw Node Data</h3>
           <div v-for="node in $store.state.nodes" :key="node.node">
             <p>{{ JSON.stringify(node) }}</p>
           </div>
-          </v-container>
-        </div>
+        </v-container>
+      </div>
       </v-container>`
 })
+
+/*
+Vue.component('node-list-node-display-name', {
+    name: "node-list-node-display-name",
+    props: ['id'],
+    data: () => ({
+        label: "",
+    }),
+    computed: {
+        nodeId: function () {
+            return parseInt(this.id.substr(0, 4), 16);
+        },
+        displayName: function () {
+            return this.$store.state.layout.nodeDetails[this.id].name
+        },
+        displayColour: function () {
+            return this.$store.state.layout.nodeDetails[this.id].colour + '--text'
+        }
+    },
+    template: `
+      <v-container>
+      <div v-if="id in $store.state.layout.nodeDetails" :class="displayColour">{{ displayName }}</div>
+      <div v-else class="black--text">Node : {{ nodeId }} </div>
+      </v-container>`
+})*/
