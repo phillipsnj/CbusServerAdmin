786428cddb1c83f78e43a1118ab8cd4e97814b84
phillipsnj <nigeljphillips@yahoo.co.uk>
2020-11-01 23:40:09 +0000

    Updates and Bug Fixes

--- public/components/nodes_list.js
+++ public/components/nodes_list.js
@@ -7,24 +7,78 @@ Vue.component('nodes-list', {
                 {text: 'node', value: 'node'},
                 {text: 'Name', value: 'name'},
                 {text: 'Group', value: 'group'},
-                {text: 'manuf', value: 'manuf'},
                 {text: 'module', value: 'module'},
-                {text: 'consumer', value: 'consumer'},
-                {text: 'producer', value: 'producer'},
                 {text: 'flim', value: 'flim'},
                 {text: 'status', value: 'status'},
-                {text: 'coe', value: 'coe'},
                 {text: 'Actions', value: 'actions', sortable: false}
             ],
             search: '',
             dialog: false,
             nodeComponent: 'mergDefault',
-            selectedNode: {}
+            selectedNode: {},
+            node_list: []
         }
+    },
+    mounted() {
+        //this.node_list = Object.values(this.$store.state.nodes)
+        this.update_node_list()
+        /*for (let node of Object.values(this.$store.state.nodes)){
+            let new_node = {}
+            new_node.node = node.node
+            new_node.module = node.module
+            new_node.flim = node.flim
+            new_node.status = node.status
+            if (node.node in this.$store.state.layout.nodeDetails) {
+                new_node.name = this.$store.state.layout.nodeDetails[node.node].name
+                new_node.group = this.$store.state.layout.nodeDetails[node.node].group
+                new_node.colour = this.$store.state.layout.nodeDetails[node.node].colour
+            } else {
+                new_node.name = `Node ${node.node}`
+                new_node.group = ''
+                new_node.colour = 'black'
+            }
+            console.log(`Node : ${node.node}`)
+            this.node_list.push(new_node)
+        }*/
+    },
+    watch: {
+        nodes() {
+            console.log('Nodes have changed')
+            this.update_node_list()
+        }
+    },
+    computed: {
+        nodeId: function () {
+            return this.$store.state.selected_node_id
+        },
+        nodes: function () {
+            return this.$store.state.nodes
+        },
     },
     methods: {
         QNN: function () {
             this.$root.send('QUERY_ALL_NODES')
+        },
+        update_node_list: function () {
+            this.node_list = []
+            for (let node of Object.values(this.$store.state.nodes)){
+                let new_node = {}
+                new_node.node = node.node
+                new_node.module = node.module
+                new_node.flim = node.flim
+                new_node.status = node.status
+                if (node.node in this.$store.state.layout.nodeDetails) {
+                    new_node.name = this.$store.state.layout.nodeDetails[node.node].name
+                    new_node.group = this.$store.state.layout.nodeDetails[node.node].group
+                    new_node.colour = this.$store.state.layout.nodeDetails[node.node].colour
+                } else {
+                    new_node.name = `Node ${node.node}`
+                    new_node.group = ''
+                    new_node.colour = 'black'
+                }
+                console.log(`Node : ${node.node}`)
+                this.node_list.push(new_node)
+            }
         },
         editNode(node) {
             console.log(`Edit Node ${node.component} : ${node.node}`)
@@ -48,13 +102,13 @@ Vue.component('nodes-list', {
     template: `
       <v-container>
       <v-data-table :headers="headers"
-                    :items="Object.values($store.state.nodes)"
+                    :items="node_list"
                     item-key="node"
                     class="elevation-1"
                     :search="search">
         <template v-slot:top>
           <v-toolbar flat>
-            <v-toolbar-title>Modules</v-toolbar-title>
+            <v-toolbar-title>Modules</v-toolbar-title>  
             <v-spacer></v-spacer>
             <v-text-field
                 v-model="search"
@@ -89,7 +143,8 @@ Vue.component('nodes-list', {
       <div>
         <v-container v-if="$store.state.debug">
           <h3>Raw Node Data</h3>
-          <div v-for="node in $store.state.nodes" :key="node.node">
+          <p>{{ node_list }}</p>
+          <div v-for="node in node_list" :key="node.node">
             <p>{{ JSON.stringify(node) }}</p>
           </div>
         </v-container>
@@ -97,27 +152,3 @@ Vue.component('nodes-list', {
       </v-container>`
 })
 
-/*
-Vue.component('node-list-node-display-name', {
-    name: "node-list-node-display-name",
-    props: ['id'],
-    data: () => ({
-        label: "",
-    }),
-    computed: {
-        nodeId: function () {
-            return parseInt(this.id.substr(0, 4), 16);
-        },
-        displayName: function () {
-            return this.$store.state.layout.nodeDetails[this.id].name
-        },
-        displayColour: function () {
-            return this.$store.state.layout.nodeDetails[this.id].colour + '--text'
-        }
-    },
-    template: `
-      <v-container>
-      <div v-if="id in $store.state.layout.nodeDetails" :class="displayColour">{{ displayName }}</div>
-      <div v-else class="black--text">Node : {{ nodeId }} </div>
-      </v-container>`
-})*/
