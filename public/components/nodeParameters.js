5d2e445350cca51c853f0ff394dc55dac5dd218c
david <david@ellis128.co.uk>
2020-11-02 09:24:44 +0000

    Added nodeParameters.js component

---
+++ public/components/nodeParameters.js
@@ -0,0 +1,94 @@
+Vue.component('nodeParameters', {
+    name: "nodeParameters",
+    mounted() {
+        this.$root.send('REQUEST_ALL_NODE_PARAMETERS', {"nodeId": this.node.node, "parameters": 20, "delay":30})
+        console.log(`nodeParamters : ${this.nodeId}`)
+    },
+    computed: {
+
+        nodeId: function () {
+            return this.$store.state.selected_node_id
+        },
+        node: function () {
+            return this.$store.state.nodes[this.$store.state.selected_node_id]
+        },
+        moduleVersion: function () {
+            return `${this.node.parameters[7]}.${String.fromCharCode(this.node.parameters[2])}`
+        }
+    },
+    methods: {
+		update() {}
+	},
+    template: `
+      <div>
+      <!--<h1>Node Parameters {{ nodeId }}</h1>--> 
+      <p v-if="$store.state.debug">{{ node.parameters }}</p>
+      <v-container>
+        <v-row>
+          <nodeValue name="NodeId" :value="nodeId"></nodeValue>
+          <nodeParameter :nodeId="nodeId" parId="1" name="Manufacturer Id"></nodeParameter>
+          <nodeValue name="Module" :value="node.module"></nodeValue>
+          <nodeValue name="Version" :value="moduleVersion"></nodeValue>
+          <nodeValue name="Flim" :value="node.flim"></nodeValue>
+          <nodeValue name="Consumer" :value="node.consumer"></nodeValue>
+          <nodeValue name="Producer" :value="node.producer"></nodeValue>
+          <nodeValue name="Learn" :value="node.learn"></nodeValue>
+          <nodeParameter :nodeId="nodeId" parId="6" name="Variables"></nodeParameter>
+          <nodeParameter :nodeId="nodeId" parId="5" name="Event Variables"></nodeParameter>
+          <nodeParameter :nodeId="nodeId" parId="4" name="Supported Events"></nodeParameter>
+          <nodeValue name="Stored Events" :value="node.EvCount"></nodeValue>
+          <nodeValue name="Actual Events" :value="Object.values(node.actions).length"></nodeValue>
+        </v-row>
+      </v-container>
+      </div>`
+})
+
+Vue.component('nodeDetail', {
+    name: "nodeParameter",
+    props: ['nodeId', 'parId', 'name'],
+    template: `
+      <div>
+      <v-card class="xs6 md3 pa-3" flat>
+        <v-text-field
+            :label="name"
+            v-model="$store.state.nodes[nodeId].parameters[parId]"
+            outlined
+            readonly
+        >
+        </v-text-field>
+      </v-card>
+      </div>`
+})
+
+Vue.component('nodeParameter', {
+    name: "nodeParameter",
+    props: ['nodeId', 'parId', 'name'],
+    template: `
+      <div>
+      <v-card class="xs6 md3 pa-3" flat>
+        <v-text-field
+            :label="name"
+            v-model="$store.state.nodes[nodeId].parameters[parId]"
+            outlined
+            readonly
+        >
+        </v-text-field>
+      </v-card>
+      </div>`
+})
+
+Vue.component('nodeValue', {
+    name: "nodeValue",
+    props: ['name', 'value'],
+    template: `
+      <div>
+      <v-card class="xs6 md3 pa-3" flat>
+        <v-text-field
+            :label="name"
+            readonly
+            outlined
+            :value=value>
+        </v-text-field>
+      </v-card>
+      </div>`
+})
