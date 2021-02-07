f8991cafbcf300394376e1d19ebc55a8829a45ed
david <david@ellis128.co.uk>
2021-02-07 20:04:37 +0000

    Starting converting programNode to accept hexdata instead of filename

--- public/components/mergCANACC5.js
+++ public/components/mergCANACC5.js
@@ -30,7 +30,10 @@ Vue.component('merg-canacc5', {
             this.$root.send('CLEAR_NODE_EVENTS', {'nodeId': this.nodeId})
             this.$root.send('REQUEST_ALL_NODE_EVENTS', {"nodeId": this.nodeId})
             this.$store.state.node_component = "merg-canacc5-node-events"
-        }
+        },
+        getFirmware() {
+            this.$store.state.node_component = "firmware"
+        },
         
     },
     template: `
@@ -41,6 +44,7 @@ Vue.component('merg-canacc5', {
           <v-tab @click="getParameters()">Parameters</v-tab>
           <v-tab @click="getVariables()" v-if="node.flim">Variables</v-tab>
           <v-tab @click="getEvents()">Stored Events</v-tab>
+          <v-tab @click="getFirmware()">Firmware</v-tab>
         </v-tabs>
         <!-- actual component selected from tab options now in node_component, so display it -->
         <component v-bind:is="$store.state.node_component"></component>
